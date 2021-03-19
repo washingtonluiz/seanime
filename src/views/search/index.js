da51944bf1ea692986ad5b3874b9b720e01108e7
Washington <washingtonluiz81@yahoo.com.br>
2021-03-18 23:06:26 -0300

    Feito ajuste na pagina de busca quando favorita e desfavorita

--- src/views/search/index.js
+++ src/views/search/index.js
@@ -1,23 +1,70 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import { withRouter } from "react-router-dom";
 
 //Components
 import Card from "../../components/card";
 import Placeholder from "../../components/card/placeholder";
 
+//redux
+import { toggleFavorite } from "../../actions";
+
 class Search extends Component {
   constructor() {
     super();
     this.state = {
       loadIcon: true,
+      animeList: [],
+      animesFavorites: [],
     };
   }
+
+  isFavorite = (id) => {
+    const { favorite } = this.props;
+
+    const isFavorite = favorite.filter((item) => {
+      return item.id === id;
+    });
+    if (isFavorite.length > 0) {
+      return true;
+    }
+    return false;
+  };
+
+  toggleFavoriteFunction = async (id) => {
+    const { animesFavorites } = this.state;
+    const { favorite, anime } = this.props;
+
+    // Verifica se o anime escolhido já está favoritado
+    const getAnime = await anime.filter((item) => {
+      return item.id === id;
+    });
+
+    // Verifica se o anime escolhido já está favoritado
+    const isFavored = await favorite.filter((item) => {
+      return item.id === id;
+    });
+
+    //Desfavorita e retorna o restante dos favoritos
+    const disfavor = await favorite.filter((item) => {
+      return item.id !== id;
+    });
+
+    if (isFavored.length === 0) {
+      let newList = animesFavorites.concat(getAnime);
+      await this.setState({ animesFavorites: newList });
+      await this.props.toggleFavorite(newList);
+    } else {
+      await this.setState({ animesFavorites: disfavor });
+      await this.props.toggleFavorite(disfavor);
+    }
+  };
 
   componentDidMount() {
     setTimeout(() => {
       const { anime = {}, history } = this.props;
-      if (anime.length < 1) {
+      if (anime.length === 0) {
         history.push("/");
       } else {
         this.setState({ loadIcon: false });
@@ -45,6 +92,8 @@ class Search extends Component {
                 <Card
                   key={item.id}
                   id={item.id}
+                  isFavorited={this.isFavorite(item.id)}
+                  toggleFavorite={(e) => this.toggleFavoriteFunction(e)}
                   {...item.attributes.posterImage}
                   {...item.attributes}
                 />
@@ -59,6 +108,10 @@ class Search extends Component {
 
 const mapStateToProps = (store) => ({
   anime: store.anime.search,
+  favorite: store.anime.favorites,
 });
 
-export default withRouter(connect(mapStateToProps)(Search));
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ toggleFavorite }, dispatch);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Search));
